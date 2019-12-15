5c05f7f55bed233360344f99a7a4fc18cc2b66eb
ducaale <sharaf.13@hotmail.com>
2019-12-15 22:51:49 +0000

    retry if vlc-server is not started

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::error::Error;
+use reqwest::Error;
 use serde::Deserialize;
 use serde_json::Value;
 use std::thread::sleep;
 use std::time::Duration;
 use std::io;
 use std::io::prelude::*;
+use std::fmt;
 
 #[derive(Deserialize, Debug)]
 struct Status {
@@ -17,18 +18,25 @@ struct Meta {
     duration: u32
 }
 
-pub struct Credentials<'a> {
+struct Credentials<'a> {
     user: &'a str,
     password: &'a str
 }
 
-fn get_meta(client: &reqwest::Client, credentials: &Credentials) -> Result<Meta, Box<dyn Error>> {
+struct Time(u32);
+impl fmt::Display for Time {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.0 / 60, self.0 % 60)
+    }
+}
+
+fn get_meta(client: &reqwest::Client, credentials: &Credentials) -> Result<Meta, Error> {
     let mut resp = client
         .get("http://localhost:8080/requests/playlist.json")
         .basic_auth(credentials.user, Some(credentials.password))
         .send()?;
 
-    let data: Value = serde_json::from_str(&resp.text()?)?;
+    let data: Value = serde_json::from_str(&resp.text().unwrap()).unwrap();
     let name = &data["children"][0]["children"][0]["name"];
     let uri = &data["children"][0]["children"][0]["uri"];
     let duration = &data["children"][0]["children"][0]["duration"];
@@ -43,7 +51,7 @@ fn get_meta(client: &reqwest::Client, credentials: &Credentials) -> Result<Meta,
 fn get_status(
     client: &reqwest::Client,
     credentials: &Credentials
-) -> Result<Status, Box<dyn Error>> {
+) -> Result<Status, Error> {
     let mut resp = client
         .get("http://localhost:8080/requests/status.json")
         .basic_auth(credentials.user, Some(credentials.password))
@@ -57,7 +65,7 @@ fn seek_to(
     client: &reqwest::Client,
     credentials: &Credentials,
     position: u32
-) -> Result<(), Box<dyn Error>> {
+) -> Result<(), Error> {
     client
         .get("http://localhost:8080/requests/status.json")
         .query(&[("command", "seek"), ("val", &position.to_string())])
@@ -71,7 +79,7 @@ fn _set_volume(
     client: &reqwest::Client,
     credentials: &Credentials,
     level: u32
-) -> Result<(), Box<dyn Error>> {
+) -> Result<(), Error> {
     client
         .get("http://localhost:8080/requests/status.json")
         .query(&[("command", "volume"), ("val", &level.to_string())])
@@ -81,25 +89,37 @@ fn _set_volume(
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let credentials = Credentials { user: "", password: "12345" };
+fn control_vlc(client: &reqwest::Client, credentials: &Credentials) -> Result<(), Error> {
+    // TODO: read skip_times from files in a function and implement memo so we
+    // don't read the same file again and again
     let skip_times = vec![(10, 15), (20, 25), (40, 45)];
-    let client = reqwest::Client::new();
 
     let meta = get_meta(&client, &credentials)?;
-    println!("Currently playing: {} duration: {} seconds", meta.name, meta.duration);
-    println!("Searching for commands file: {}", meta.uri);
+    let status = get_status(&client, &credentials)?;
+    print!("\r[info] Currently playing: {} ({} / {})", meta.name, Time(status.time), Time(meta.duration));
+    io::stdout().flush().unwrap();
 
-    loop {
-        let status = get_status(&client, &credentials)?;
-        print!("\rCurrent time: {} seconds", status.time);
-        io::stdout().flush()?;
-
-        for &(skip_start, skip_end) in skip_times.iter() {
-            if status.time >= skip_start && status.time < skip_end {
-                seek_to(&client, &credentials, skip_end)?;
-            }
+    for &(skip_start, skip_end) in skip_times.iter() {
+        if status.time >= skip_start && status.time < skip_end {
+            seek_to(&client, &credentials, skip_end)?;
         }
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), reqwest::Error> {
+    let credentials = Credentials { user: "", password: "12345" };
+    let client = reqwest::Client::new();
+    loop {
+        if let Err(e) = control_vlc(&client, &credentials) {
+            if e.status() == None {
+                print!("\n[err] Unable to connect to vlc")
+            }
+            else {
+                return Err(e);
+            }
+        };
         sleep(Duration::from_millis(500));
     }
 }
